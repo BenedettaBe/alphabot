9e9d5ceabf903f93cca0e0c21556b2a164c12117
BenedettaBe <144808596+BenedettaBe@users.noreply.github.com>
2024-12-16 13:31:49 +0100

    add code for site to control the robot

---
+++ robotSito/robot.py
@@ -0,0 +1,29 @@
+from AlphaBot import AlphaBot
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+robot = AlphaBot()
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if request.form.get('W') == 'W':
+            print("avanti")
+            robot.forward()
+        elif  request.form.get('S') == 'S':
+            robot.backward()
+        elif request.form.get('A') == 'A':
+            robot.left()
+        elif  request.form.get('D') == 'D':
+            robot.right()
+        elif  request.form.get('STOP') == 'STOP':
+            robot.stop()
+        else:
+            print("Unknown")
+    elif request.method == 'GET':
+        return render_template('index.html')
+    
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='192.168.1.130')
